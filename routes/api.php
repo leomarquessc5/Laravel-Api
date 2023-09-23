3ff8fdacedb83c89900749a8c1aaad2879b65421
Leonardo M <leomarquessc5@gmail.com>
2023-09-22 23:42:46 -0300

    fix: tests

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
 
-// Route::get('/', function () {
-//     return response('Success', 200);
-// });
+Route::get('/', function () {
+    return response('Success', 200);
+});
